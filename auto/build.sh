801552532cb968ca4b8dfd3d0ffe043e7becab0d
Hawwa11 <hawwazohair@gmail.com>
2021-02-10 21:26:55 +0800

    ..

--- auto/build.sh
+++ auto/build.sh
@@ -5,7 +5,7 @@ set -eux
 echo "🛠 Building project..."
   
   IMAGE_TAG=${BUILD_VERSION:=latest}
-  node.app.js
+  node app.js
   docker tag platform:latest ${REPOSITORY_URI}:latest
   docker tag ${REPOSITORY_URI}:latest ${REPOSITORY_URI}:${IMAGE_TAG}
   docker push ${REPOSITORY_URI}:latest
