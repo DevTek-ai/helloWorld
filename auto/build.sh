cbbb85fa87fb8dcc09536f315efb97f08b896c07
Hawwa11 <hawwazohair@gmail.com>
2021-02-10 19:47:33 +0800

    build file

--- auto/build.sh
+++ auto/build.sh
@@ -5,7 +5,7 @@ set -eux
 echo "🛠 Building project..."
   
   IMAGE_TAG=${BUILD_VERSION:=latest}
-  ./mvnw package -Pprod -Dmaven.test.skip=true -DskipTests jib:dockerBuild  
+  node.app.js
   docker tag platform:latest ${REPOSITORY_URI}:latest
   docker tag ${REPOSITORY_URI}:latest ${REPOSITORY_URI}:${IMAGE_TAG}
   docker push ${REPOSITORY_URI}:latest
